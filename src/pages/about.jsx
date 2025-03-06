baaa5f619aef4169b6a66569733cf0b76d1bfe39
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-03-07 00:22:05 +0330

    build about page design and fix home pageug

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,27 @@
+import {Link} from "react-router-dom";
+
 function About() {
     return (
-        <section>
-            about
+        <section className={'w-[80%] flex items-center justify-center m-auto'}>
+            <div className={'container mx-auto px-4 max-w-4xl'}>
+                <h1 className={'text-4xl font-extrabold text-gray-900 mb-6 text-center'}>About This Website</h1>
+                <p className={'text-xl text-gray-700 leading-relaxed mb-4'}>This website uses the <span
+                    className={'text-blue-600'}>REST Countries API</span> to
+                    display comprehensive information about countries from
+                    around the world.</p>
+                <p className={'text-xl text-gray-700 leading-relaxed mb-4'}>Explore our data to learn about country
+                    names,
+                    capitals, regions, populations, flags, and much more. Whether you’re curious about a particular
+                    country or looking for insights about global regions, our interactive explorer makes it easy.
+
+                </p>
+                <p className={'text-xl text-gray-700 leading-relaxed mb-4'}>Our goal is to build a fully responsive and
+                    modern web application using the latest technologies, including React Router v7 for seamless routing
+                    and Tailwind CSS for a beautiful, responsive user interface.
+
+                </p>
+            </div>
+
         </section>
     )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,13 +3,13 @@ import {Link} from "react-router-dom";
 function Home() {
     return (
         <section className={'w-[80%] m-auto'}>
-            <div className={'w-1/3 space-y-5'}>
-                <h1 className={'font-bold text-4xl'}>Explore Countries with<span
+            <div className={'w-1/2  space-y-5'}>
+                <h1 className={'text-4xl  tracking-tight text-gray-900 sm:text-5xl'}>Explore Countries with<span
                     className={'text-[#2563EB]'}>Real-Time Data</span></h1>
                 <p className={'text-[20px] text-gray-900'}>Discover details about every country around the world – from capitals to regions!</p>
-                <div className={'space-x-4'}>
-                    <Link className={'inline-block bg-[#2563EB] text-white py-3 px-6 rounded-md hover:bg-blue-700 transition-all'} to="/countries">Explore Now</Link>
-                    <Link className={'inline-block bg-gray-200 text-gray-700 py-3 px-6 rounded-md hover:bg-gray-300 transition-all'} to="/about">Learn More</Link>
+                <div className={'flex space-x-4'}>
+                    <Link className={'flex items-center justify-center px-6 py-3 text-lg text-white bg-blue-600 rounded-md hover:bg-blue-700'} to="/countries">Explore Now</Link>
+                    <Link className={'flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600'} to="/about">Learn More</Link>
                 </div>
             </div>
 
